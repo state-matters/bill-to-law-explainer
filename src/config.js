bd64891eb53dd078439d0b201ddb82e0ce541251
Wade Novak <wade@launchpadlab.com>
2019-02-16 11:23:56 -0600

    First draft of happy path data object

--- src/config.js
+++ src/config.js
@@ -12,6 +12,41 @@ export const STEPS = [
   {
     key: "ideaBecomesABill",
     title: "Your idea becomes a bill.",
-    subTitle: "After a bill becomes..."
-  }
+    subTitle: "After a bill is written, it needs a sponsor. The legislator that sponsors the bill then introduces it to their chamber. Senators introduce bills in the Senate and Representatives introduce bills in the House."
+  },
+  {
+    key: "houseRulesCommittee",
+    title: "Welcome to the House Rules Committee.",
+    subTitle: "In the House, a bill's first hurdle is the Rules Committee. This committee can either kill the bill right from the start by stalling it indefinitely, or move it to the next stage by assigning it to one or more House Committees related to the bill's subject matter (for example, insurance, labor, or public health). The Rules Committee is made up of three members from the majority party and two from the minority party."
+  },
+  {
+    key: "specificHouseCommittee",
+    title: "Your bill has been assigned to a House committee.",
+    subTitle: "If a bill reaches committee, the fun begins! Committee members question the bill's sponsor and weigh whether or not to send the bill to the House floor for further debate amendements. Hearings are held where citizens, interest groups, and lobbyists can all weigh in. With a simple majority vote in committee, the bill passes to the full House!"
+  },
+  {
+    key: "houseFloor",
+    title: "Your bill has reached the House floor.",
+    subTitle: "The bill is read on the House floor and amendments are proposed if needed. The bill is read once more, after which, the House votes. If it fails, the bill is DEAD. If it passes a majority vote, the bill is sent to ... THE SENATE!"
+  },
+  {
+    key: "senantAssignmentsCommittee",
+    title: "Welcome to the Senate Assignments Committee.",
+    subTitle: "Now the bill has a Senate sponsor. The Assignments Committee (like the Rules Committee in the House) assigns the bill to the appropriate Senate committee."
+  },
+  {
+    key: "specificSenateCommittee",
+    title: "Your bill has been assigned to a Senate committee.",
+    subTitle: "Just like in the House, the Senate decides if it wants to make any amendments. Then the committee votes on whether to pass the bill to the full Senate."
+  },
+  {
+    key: "senateFloor",
+    title: "Your bill has reached the Senate floor.",
+    subTitle: "On the floor, Senators can further amend the bill. If it passes without the Senate (or Senate committee) amending it, then it can go straight to the Governor. If it has been amended (in other words, if it's been changed from the version the House passed) those changes have to be voted on and approved by the House as well."
+  },
+  {
+    key: "governorsDesk",
+    title: "Your bill now sits on the Governor's desk.",
+    subTitle: "If the Governor approves the bill, they then sign it into law! However, the Governor can also partially or completely veto the bill. If they do, the General Assembly has one last chance to potentially save the bill. <a href='www.google.com' target='_blank'>Learn more about Illinois' veto process here</a>"
+  },
 ]
