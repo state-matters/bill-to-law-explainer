cb46c4827fbff8428d20caeeb275548938c8e7b3
Wade Novak <wade@launchpadlab.com>
2019-02-16 10:34:58 -0600

    WIP data structure for bill steps

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export const STEPS = [
+  {
+    key: "idea",
+    title: "You've got an idea for a new law.",
+    subTitle: "Every bill starts with an idea to fix a problem or make our communities better. These ideas come from legislators, constituents, activists, advocacy groups, think tanks, policy centers and more."
+  },
+  {
+    key: "legislativeReferenceBureau",
+    title: "Welcome to the Legislative Reference Bureau.",
+    subTitle: "Your idea now goes to the Legislative Reference Bureau, which writes and prepares legislation. This group translates your idea into 'legalese' while attorneys employed by the Bureau provide confidential and nonpartisan legal advice and writing services to both parties and both houses."
+  },
+  {
+    key: "ideaBecomesABill",
+    title: "Your idea becomes a bill.",
+    subTitle: "After a bill becomes..."
+  }
+]
--- src/js/App.js
+++ src/js/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import React, { Component, Fragment } from 'react';
 import '../styles/App.css';
+import { STEPS } from "../config"
+
+/*
+State:
+  activeStep
+
+Need to handle:
+  onKeyDown:
+    (parse the keycode)
+      rightArrowClick (should go to next step)
+      leftArrowClick (should go to prev step)
+*/
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Fragment>
+            <h2>{STEPS[0].title}</h2>
+            <p>{STEPS[0].subTitle}</p>
+          </Fragment>
         </header>
       </div>
     );
